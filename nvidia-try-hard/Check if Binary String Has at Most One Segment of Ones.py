7f2b21bbe13d6f9bcd00a7b117051e87f6b675cd
tuong.nguyen <richardnguyen0715@gmail.com>
2026-03-06 10:34:35 +0700

    nvidia: update 2 easy

---
+++ nvidia-try-hard/Check if Binary String Has at Most One Segment of Ones.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkOnesSegment(self, s: str) -> bool:
+        count = 0
+
+        for i in range(len(s)):
+            if s[i] == '1' and (i == 0 or s[i-1] == '0'):
+                count += 1
+                if count > 1:
+                    return False
+        
+        return True
---
+++ nvidia-try-hard/rectangleOverlap.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isRectangleOverlap(self, rec1, rec2):
+        return not (
+            rec1[2] <= rec2[0] or
+            rec1[0] >= rec2[2] or
+            rec1[3] <= rec2[1] or
+            rec1[1] >= rec2[3]
+        )
+        
+    
+    
+class Solution:
+    def isRectangleOverlap(self, rec1, rec2):
+
+        if rec1[2] <= rec2[0]:
+            return False
+        
+        if rec1[0] >= rec2[2]:
+            return False
+        
+        if rec1[3] <= rec2[1]:
+            return False
+        
+        if rec1[1] >= rec2[3]:
+            return False
+        
+        return True
