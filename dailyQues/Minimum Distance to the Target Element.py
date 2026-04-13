3f868e75c3deacab44fbdde835537e27e24d5d6b
tuong.nguyen <richardnguyen0715@gmail.com>
2026-04-13 21:21:53 +0700

    dailyQues: update 1 easy

---
+++ dailyQues/Minimum Distance to the Target Element.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        n = len(nums)
+        ans = float("inf")
+        for i in range(n):
+            if nums[i] == target:
+                ans = min(ans, abs(i - start))
+        return ans
